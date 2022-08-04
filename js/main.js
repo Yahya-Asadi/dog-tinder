128d96f03c8bb5d35ccc9f0b118265da57f06697
Yahya Asadi Sheikhi <yahya.asadi.sh@gmail.com>
2022-08-05 04:07:47 +0430

    moved data to a seperate module

--- js/main.js
+++ js/main.js
@@ -1,46 +1,28 @@
-const dogs = [
-    {
-        name: "Rex",
-        avatar: "images/dog-rex.jpg",
-        age: 25,
-        bio: "Art. Literature. Natural wine. Yoga.",
-        hasBeenSwiped: false,
-        hasBeenLiked: false
-    },{
-        name: "Bella",
-        avatar: "images/dog-bella.jpg",
-        age: 43,
-        bio: "Yup, that's my owner. U can meet him if you want",
-        hasBeenSwiped: false,
-        hasBeenLiked: false
-    },
-    {
-        name: "Teddy",
-        avatar: "images/dog-teddy.jpg",
-        age: 30,
-        bio: "How you doin?",
-        hasBeenSwiped: false,
-        hasBeenLiked: false
-    }
-]
-
+import  dogs from "/js/data.js"
 let currentDogIndex = 0
 let likeStatusChange = true
 render(dogs[currentDogIndex])
 
 function render(dog){
-	document.getElementsByClassName("container-main")[0].style.backgroundImage = `url("${dog.avatar}")`
+	document.getElementsByClassName("container-main")[0]
+				.style.backgroundImage = `url("${dog.avatar}")`
 	if (dog.hasBeenLiked) {
-		document.getElementById("accept-state-img").src="images/badge-like.png"
-		document.getElementsByClassName("accept-state")[0].style.display = "block"
+		document.getElementById("accept-state-img")
+					.src="images/badge-like.png"
+		document.getElementsByClassName("accept-state")[0]
+					.style.display = "block"
 	}
 	else if (dog.hasBeenLiked === false && dog.hasBeenSwiped) {
-		document.getElementById("accept-state-img").src="images/badge-nope.png"
-		document.getElementsByClassName("accept-state")[0].style.display = "block"
+		document.getElementById("accept-state-img")
+					.src="images/badge-nope.png"
+		document.getElementsByClassName("accept-state")[0]
+					.style.display = "block"
 	}
 	else {
-		document.getElementById("accept-state-img").src=""
-		document.getElementsByClassName("accept-state")[0].style.display = "none"
+		document.getElementById("accept-state-img")
+					.src=""
+		document.getElementsByClassName("accept-state")[0]
+					.style.display = "none"
 	}
 }
 
@@ -49,8 +31,10 @@ document.getElementById("dont-like-btn").addEventListener("click",
 		if (likeStatusChange) {
 			dogs[currentDogIndex].hasBeenSwiped = true
 			dogs[currentDogIndex].hasBeenLiked = false
-			document.getElementById("accept-state-img").src="images/badge-nope.png"
-			document.getElementsByClassName("accept-state")[0].style.display = "block"
+			document.getElementById("accept-state-img")
+						.src="images/badge-nope.png"
+			document.getElementsByClassName("accept-state")[0]
+						.style.display = "block"
 			likeStatusChange = false
 			setTimeout(
 				() => {
@@ -75,8 +59,10 @@ document.getElementById("you-like-btn").addEventListener("click",
 		if (likeStatusChange) {
 			dogs[currentDogIndex].hasBeenSwiped = true
 			dogs[currentDogIndex].hasBeenLiked = true
-			document.getElementById("accept-state-img").src="images/badge-like.png"
-			document.getElementsByClassName("accept-state")[0].style.display = "block"
+			document.getElementById("accept-state-img")
+						.src="images/badge-like.png"
+			document.getElementsByClassName("accept-state")[0]
+						.style.display = "block"
 			likeStatusChange = false
 			setTimeout(
 				() =>{
