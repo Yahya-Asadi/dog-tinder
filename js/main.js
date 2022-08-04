c6ac0681af245d95c14d4ca84df9738cf2415a44
Yahya Asadi Sheikhi <yahya.asadi.sh@gmail.com>
2022-08-04 23:16:19 +0430

    fixes images directory path

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 const teddy = {
 	name: "teddy",
 	age: 34,
-	image: "../images/dog-teddy.jpg",
+	image: "images/dog-teddy.jpg",
 	likeStatus: undefined,
 }
 
 document.getElementsByClassName("container-main")[0].style.backgroundImage = `url("${teddy.image}")`
 document.getElementById("you-like-btn").addEventListener("click", () => {
-	document.getElementById("accept-state-img").src="../images/badge-like.png"
+	document.getElementById("accept-state-img").src="images/badge-like.png"
 	document.getElementsByClassName("accept-state")[0].style.display = "block"
 }
 )
 document.getElementById("dont-like-btn").addEventListener("click", () => {
-	document.getElementById("accept-state-img").src="../images/badge-nope.png"
+	document.getElementById("accept-state-img").src="images/badge-nope.png"
 	document.getElementsByClassName("accept-state")[0].style.display = "block"
 }
 )
